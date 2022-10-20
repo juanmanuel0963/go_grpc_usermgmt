747bbab55f465db8f8762497f094c44a401534aa
juanmanuel0963 <juanmanuel0963@gmail.com>
2022-10-20 12:39:12 -0500

    commit

--- usermgmt_client/usermgmt_client.go
+++ usermgmt_client/usermgmt_client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -25,11 +26,11 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	var new_users = make(map[string]int32)
+	var new_users = make(map[string]int)
 	new_users["Alice"] = 43
 	new_users["Bob"] = 30
 	for name, age := range new_users {
-		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: int32(age)})
 		if err != nil {
 			log.Fatalf("could not create user: %v", err)
 		}
@@ -39,4 +40,11 @@ AGE: %d
 ID: %d`, r.GetName(), r.GetAge(), r.GetId())
 
 	}
+	params := &pb.GetUsersParams{}
+	r, err := c.GetUsers(ctx, params)
+	if err != nil {
+		log.Fatalf("could not create user: %v", err)
+	}
+	log.Print("\nUSER LIST: \n")
+	fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
 }
